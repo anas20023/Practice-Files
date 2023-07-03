37fc6ffd5fde81c0d0dd1e9fffb9cec64014e45a
Anas Ibn Belal <98760910+anas20023@users.noreply.github.com>
2023-07-04 04:26:06 +0600

    Add files via upload

---
+++ table.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+
+
+
+        int n;
+
+        printf("Enter a value: ");
+        scanf("%d" , &n);
+
+
+        for (int i = 1; i <=10; i++)
+        {
+            printf("%d\n", i*n);
+        }
+        
+
+
+
+
+
+
+
+
+    return 0;
+}
