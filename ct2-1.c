1da9b155e1fee68bce5d8e7c2aa94797fd02be6c
Anas Ibn Belal <98760910+anas20023@users.noreply.github.com>
2023-10-18 23:44:01 +0600

    ques 1 - CT-2

---
+++ ct2-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    int d = 40;
+
+    printf("a = %d, b = %d, c = %d, d = %d\n", a, b, c, d);
+
+    return 0;
+}
